1b2b482b94ce11f49057906397defcea6b057c65
Ajay Sharma <hi@ajaysharma.dev>
2023-10-24 22:46:31 +0530

    refactor code

--- app.js
+++ app.js
@@ -2,79 +2,89 @@ import ffmpeg from "fluent-ffmpeg";
 import fs from "fs";
 import path from "path";
 
-const VIDEO_FILE_PATH = "video.mp4";
-const SUBTITLE_FILE_PATH = "subtitle.srt";
-const OUTPUT_DIR = "output";
-
-async function splitVideo() {
-  // Ensure the output directory exists
-  if (!fs.existsSync(OUTPUT_DIR)) {
-    fs.mkdirSync(OUTPUT_DIR);
+// Function to process tasks in batches
+async function processInBatches(tasks, batchSize) {
+  let i = 0;
+  const results = [];
+  while (i < tasks.length) {
+    const batch = tasks.slice(i, i + batchSize);
+    const batchResults = await Promise.all(batch);
+    results.push(...batchResults);
+    i += batchSize;
   }
+  return results;
+}
 
-  // Get video duration
-  const videoDuration = await new Promise((resolve, reject) => {
-    ffmpeg.ffprobe(VIDEO_FILE_PATH, (err, metadata) => {
+// Function to split video
+async function splitVideo(videoFilePath, outputDir, NP) {
+  const videoDuration = await getVideoDuration(videoFilePath);
+  const numberOfClips = Math.ceil(videoDuration / 60);
+
+  const tasks = Array.from({ length: numberOfClips }, async (_, i) => {
+    const outputFilePath = path.join(outputDir, `clip_${i + 1}.mp4`);
+    await saveSegment(videoFilePath, i * 60, 60, outputFilePath);
+
+    // Display progress
+    console.log(`Processed video segment: ${i + 1}/${numberOfClips}`);
+    return outputFilePath;
+  });
+
+  const outputFilePaths = await processInBatches(tasks, NP);
+
+  return outputFilePaths;
+}
+
+async function getVideoDuration(videoFilePath) {
+  return new Promise((resolve, reject) => {
+    ffmpeg.ffprobe(videoFilePath, (err, metadata) => {
       if (err) reject(err);
       resolve(metadata.format.duration);
     });
   });
-
-  const numberOfClips = Math.ceil(videoDuration / 60);
-
-  for (let i = 0; i < numberOfClips; i++) {
-    const outputFilePath = path.join(OUTPUT_DIR, `clip_${i + 1}.mp4`);
-
-    ffmpeg(VIDEO_FILE_PATH)
-      .seekInput(i * 60)
-      .duration(60)
-      .on("end", () => {
-        console.log(`Segment ${i + 1} is finished.`);
-      })
-      .save(outputFilePath);
-  }
 }
 
-// Parse SRT file
+function saveSegment(inputFilePath, start, duration, outputFilePath) {
+  return new Promise((resolve, reject) => {
+    ffmpeg(inputFilePath)
+      .seekInput(start)
+      .duration(duration)
+      .on("end", resolve)
+      .on("error", reject)
+      .save(outputFilePath);
+  });
+}
+
+// Parse SRT data
 function parseSRT(data) {
   const blocks = data.split("\n\n");
-  let items = [];
-
-  blocks.forEach((block) => {
-    const lines = block.split("\n");
-    if (lines[1]) {
-      const time = lines[1].split(" --> ");
+  return blocks
+    .map((block) => {
+      const lines = block.split("\n");
+      const [start, end] = lines[1]?.split(" --> ") || [];
       const text = lines.slice(2).join(" ");
-
-      items.push({ start: time[0], end: time[1], text: text });
-    }
-  });
-
-  return items;
+      return { start, end, text };
+    })
+    .filter((item) => item.start && item.end && item.text);
 }
 
-// Split video by 30 seconds
 function splitByTime(items, timeInSecs) {
   let segments = [];
   let start = items[0].start;
 
   for (let i = 0; i < items.length; i++) {
-    const item = items[i];
-    if (timeDiffInSeconds(start, item.end) > timeInSecs) {
-      segments.push({ start: start, end: item.end });
-      start = item.end;
+    if (timeDiffInSeconds(start, items[i].end) > timeInSecs) {
+      segments.push({ start, end: items[i].end });
+      start = items[i].end;
     }
 
-    // Push the last segment
     if (i === items.length - 1) {
-      segments.push({ start: start, end: item.end });
+      segments.push({ start, end: items[i].end });
     }
   }
 
   return segments;
 }
 
-// Calculate time difference in seconds
 function timeDiffInSeconds(start, end) {
   const parseTime = (time) => {
     const [h, m, s] = time.split(":");
@@ -89,10 +99,10 @@ function timeDiffInSeconds(start, end) {
 
   return parseTime(end) - parseTime(start);
 }
-function splitSubtitles(data) {
+function splitSubtitles(data, outputDir) {
   const items = parseSRT(data);
   const segments = splitByTime(items, 60);
-  let segmentFiles = [];
+  const segmentFiles = [];
 
   segments.forEach((segment, index) => {
     const segmentSubtitles = items.filter(
@@ -101,28 +111,46 @@ function splitSubtitles(data) {
         timeDiffInSeconds(segment.end, item.end) <= 0
     );
 
-    if (segmentSubtitles.length > 0) {
-      let srtContent = "";
-      segmentSubtitles.forEach((subtitle, subIndex) => {
-        srtContent += `${subIndex + 1}\n${subtitle.start} --> ${
-          subtitle.end
-        }\n${subtitle.text}\n\n`;
-      });
+    if (segmentSubtitles.length) {
+      const srtContent = segmentSubtitles
+        .map(
+          (subtitle, subIndex) =>
+            `${subIndex + 1}\n${subtitle.start} --> ${subtitle.end}\n${
+              subtitle.text
+            }`
+        )
+        .join("\n\n");
 
       const segmentFilePath = path.join(
-        OUTPUT_DIR,
+        outputDir,
         `subtitle_segment_${index + 1}.srt`
       );
-      fs.writeFileSync(segmentFilePath, srtContent.trim());
+      fs.writeFileSync(segmentFilePath, srtContent);
       segmentFiles.push(segmentFilePath);
+
+      // Display progress
+      console.log(
+        `Processed subtitle segment: ${index + 1}/${segments.length}`
+      );
     }
   });
 
   return segmentFiles;
 }
 
-// Run the functions
-splitVideo();
-const subtitleData = fs.readFileSync(SUBTITLE_FILE_PATH, "utf-8");
-const segments = splitSubtitles(subtitleData);
-console.log(`Split subtitles saved at: ${segments.join(", ")}`);
+// Run functions
+(async () => {
+  const VIDEO_FILE_PATH = "video.mp4";
+  const SUBTITLE_FILE_PATH = "subtitle.srt";
+  const OUTPUT_DIR = "output";
+
+  if (!fs.existsSync(OUTPUT_DIR)) {
+    fs.mkdirSync(OUTPUT_DIR);
+  }
+
+  await splitVideo(VIDEO_FILE_PATH, OUTPUT_DIR, 8);
+  const subtitleData = fs.readFileSync(SUBTITLE_FILE_PATH, "utf-8");
+  const segments = splitSubtitles(subtitleData, OUTPUT_DIR);
+
+  console.log(`Split subtitles saved at: ${segments.join(", ")}`);
+})();
